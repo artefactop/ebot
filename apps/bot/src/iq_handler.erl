1121af9ef2cde6034a68fb6c194b49b5cc0da200
Pepe <pepe@yuilop.com>
2013-03-20 19:02:03 +0100

    Bug fix

--- apps/bot/src/iq_handler.erl
+++ apps/bot/src/iq_handler.erl
@@ -12,19 +12,19 @@ handle_ping(Session, "get", Packet) ->
     Result = exmpp_iq:result(Packet),
     exmpp_session:send_packet(Session, Result);
 handle_ping(_Session, "result", Packet) ->
-    lager:info("Received result ping IQ:~n~p~n", [Packet]),
+    lager:debug("Received result ping IQ:~n~p~n", [Packet]),
     Id = exmpp_xml:get_attribute(Packet, <<"id">>, <<"unknown">>),
-    case ping_timer:remove(Id) of 
+    case timem:remove(Id) of 
         {_, {Service, Tm}} ->
             bot:syslog(notice, io_lib:format("Service ~p responding in ~p milliseconds", [erlang:binary_to_list(Service), diff_now_ms(Tm)]));
         _ ->
             bot:syslog(warning, io_lib:format("Unknown :~p", [Packet]))
     end;
 handle_ping(_Session, "error", Packet) ->
-    lager:info("Received error ping IQ:~n~p~n", [Packet]),
+    lager:debug("Received error ping IQ:~n~p~n", [Packet]),
     Id = exmpp_xml:get_attribute(Packet, <<"id">>, <<"unknown">>),
     Condition = exmpp_stanza:get_condition(Packet),
-    case ping_timer:remove(Id) of 
+    case timem:remove(Id) of 
         {_, {Service, Tm}} ->
             bot:syslog(emerg, io_lib:format("Service ~p responding error ~p in ~p milliseconds", [erlang:binary_to_list(Service), erlang:atom_to_list(Condition), diff_now_ms(Tm)]));
         _ ->
@@ -32,4 +32,4 @@ handle_ping(_Session, "error", Packet) ->
     end.
 
 diff_now_ms(T) ->
-    (ping_timer:tm(os:timestamp()) - ping_timer:tm(T))/1000.
+    (timem:tm(os:timestamp()) - timem:tm(T))/1000.
